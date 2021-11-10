4cc36657f5af8ea31a26d64514d7ad3fb721b35b
JuezRafecas <gijonrafaelantonio@gmail.com>
2021-11-10 13:05:43 -0300

    v2

--- js/agregarMesas.js
+++ js/agregarMesas.js
@@ -105,12 +105,10 @@ window.onload = () =>{
     }
 
 
-
-
-
-
-
-
-
-
+    const formularioListaEspera = document.querySelector('.guest-list');
+    formularioListaEspera.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        let maxLista = document.querySelector('.lista h3 span');
+        maxLista.innerText = `(Max: ${parseInt(document.querySelector('.guest-list div input').value)})`;
+    })
 }
